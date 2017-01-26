afc7ac1571490d8dbc2b7d96b26b356b7fb9f865
Breno Azevedo <maddox.br@gmail.com>
2017-01-26 16:24:57 -0200

    * Refactored all `foreach` occurrences by `for`, to optimize recursive performance * Some code cleanup

--- Assets/EzMsg/EzMsg.cs
+++ Assets/EzMsg/EzMsg.cs
@@ -9,20 +9,10 @@ using Object = UnityEngine.Object;
 
 namespace Ez.Msg
 {
-
     public static class EzMsg  {
 
-//	private static EzMsg _msg = null;
-//	public static EzMsg Data {
-//		get {
-//			if (_msg == null)
-//				_msg = new EzMsg();
-//			return _msg;
-//		}
-//	}
-
-        private static MonoBehaviour _manager;
         /// <summary> Singleton used to hold all coroutines used by EzMsg</summary>
+        private static MonoBehaviour _manager;
         public static MonoBehaviour Manager {
             get
             {
@@ -64,8 +54,9 @@ namespace Ez.Msg
 
             private IEnumerator RunCoroutines(List<IEnumerable> funcs)
             {
-                foreach (var func in funcs)
+                for (var i = 0; i < funcs.Count; i++)
                 {
+                    var func = funcs[i];
                     //Debug.Log(func.ToString());
                     if (func != null)
                         yield return Manager.StartCoroutine(func.GetEnumerator());
@@ -117,8 +108,8 @@ namespace Ez.Msg
                 return response;
 
             // GetComponentsInChildren(false) = exclude inactive
-            foreach (Transform child in target.GetComponentsInChildren<Transform>(false))
-            {
+            for (var i = 0; i < target.GetComponentsInChildren<Transform>(false).Length; i++) {
+                Transform child = target.GetComponentsInChildren<Transform>(false)[i];
                 // Skips `target` so it isn't requested twice
                 if (child == target.transform)
                     continue;
@@ -157,41 +148,27 @@ namespace Ez.Msg
         private static IEnumerable WaitCoroutine(float waitTime, bool realtime = false)
         {
               #if UNITY_5_4_OR_NEWER
-//            if (realtime)
-//                yield return new WaitForSecondsRealtime(waitTime);
-//            else
+              if (realtime)
+                 yield return new WaitForSecondsRealtime(waitTime);
+              else
               #endif
                 yield return new WaitForSeconds(waitTime);
         }
 
         //     ### Variation with C# 'Timers'
-
 //        var startTime = realtime ? Time.realtimeSinceStartup : Time.time;
-
 //        yield return new WaitWhile(() => Time.time < startTime + waitTime );
-
 //        var timer = new Timer(delay);
-
 //        timer.Elapsed += (_, __) =>
-
 //        {
-
 //            Debug.Log("Sending message to " + typeof(T) + " after " + delay / 1000 + " s.");
-
 //            // Fire Event
-
 //            //ExecuteRecursive(target, functor, sendToChildren, sendSeqData);
-
 //            callbackAction.SafeInvoke();
-
 //            timer.Stop();
-
 //            timer.Dispose();
-
 //        };
-
 //        timer.Start();
-
 
         public static IEnumerable ExecuteRecursive<T>(GameObject target, EventAction<T> functor,
             bool sendToChildren = false)
@@ -204,7 +181,9 @@ namespace Ez.Msg
 
             // Will wait for the logic completion of all valid targets in the target's hierarchy
             // (false) = don't include inactive
-            foreach (Transform child in target.GetComponentsInChildren<Transform>(false)) {
+            for (var i = 0; i < target.GetComponentsInChildren<Transform>(false).Length; i++)
+            {
+                Transform child = target.GetComponentsInChildren<Transform>(false)[i];
                 // Skips `target` so it isn't requested twice
                 if (child == target.transform)
                     continue;
@@ -255,188 +234,11 @@ namespace Ez.Msg
             return EqualityComparer<T2>.Default.Equals(requestValue, default(T2));
         }
 
-        #region #Region ##### Additions to original ExecuteEvents #####
+        #region #Region ##### Additions / Tweaks to original ExecuteEvents #####
         /// Adapted from //https://github.com/tenpn/unity3d-ui/blob/master/UnityEngine.UI/EventSystem/ExecuteEvents.cs
 
         public delegate IEnumerable EventAction<T1>(T1 handler);
         public delegate T2 EventFunc<T1,T2>(T1 handler);
-
-//    public static T ValidateEventData<T>(BaseEventData data) where T : class
-//    {
-//        if ((data as T) == null)
-//            throw new ArgumentException(String.Format("Invalid type: {0} passed to event expecting {1}", data.GetType(), typeof(T)));
-//        return data as T;
-//    }
-
-        private static void GetEventChain(GameObject root, IList<Transform> eventChain)
-        {
-            eventChain.Clear();
-            if (root == null)
-                return;
-
-            var t = root.transform;
-            while (t != null)
-            {
-                eventChain.Add(t);
-                t = t.parent;
-            }
-        }
-
-        private static readonly ObjectPool<List<IEventSystemHandler>> s_HandlerListPool = new ObjectPool<List<IEventSystemHandler>>(null, l => l.Clear());
-
-        public static bool Execute<T>(GameObject target, EventAction<T> functor) where T : IEventSystemHandler
-        {
-            var internalHandlers = s_HandlerListPool.Get();
-            GetEventList<T>(target, internalHandlers);
-            //	if (s_InternalHandlers.Count > 0)
-            //		Debug.Log("Executing " + typeof (T) + " on " + target);
-
-            for (var i = 0; i < internalHandlers.Count; i++)
-            {
-                T arg;
-                try
-                {
-                    arg = (T)internalHandlers[i];
-                }
-                catch (Exception e)
-                {
-                    var temp = internalHandlers[i];
-                    Debug.LogException(new Exception(string.Format("Type {0} expected {1} received.", typeof(T).Name, temp.GetType().Name), e));
-                    continue;
-                }
-
-                try
-                {
-                    functor(arg);
-                }
-                catch (Exception e)
-                {
-                    Debug.LogException(e);
-                }
-            }
-
-            var handlerCount = internalHandlers.Count;
-            s_HandlerListPool.Release(internalHandlers);
-            return handlerCount > 0;
-        }
-
-        /// <summary>
-        /// A Request sends a message asking for the second parameter type as return, instead of a success/failure bool
-        /// For primitives you must use, eg. bool? or int? in the method call to get a null as return, if no answer found
-        /// </summary>
-        /// <param name="target">Target GameObject to send the request</param>
-        /// <param name="eventData">Event Data</param>
-        /// <param name="functor">Actual Method to be executed</param>
-        /// <typeparam name="T1">Functor type</typeparam>
-        /// <typeparam name="T2">Return Type</typeparam>
-        /// <returns></returns>
-        public static T2 Request<T1,T2>(GameObject target, BaseEventData eventData, EventFunc<T1,T2> functor)
-            where T1 : IEventSystemHandler
-        {
-            var internalHandlers = s_HandlerListPool.Get();
-            GetEventList<T1>(target, internalHandlers);
-            // Debug Purposes Only
-//            Debug.Log(internalHandlers.Count + " scripts found of type " + typeof (T1) + " on " + target.name);
-//
-//            if (internalHandlers.Count > 0)
-//                Debug.Log("Executing " + typeof (T1) + " on " + target);
-
-            T2 returnValue = default(T2);
-            for (var i = 0; i < internalHandlers.Count; i++)
-            {
-                T1 arg;
-                try
-                {
-                    arg = (T1)internalHandlers[i];
-                }
-                catch (Exception e)
-                {
-                    var temp = internalHandlers[i];
-                    Debug.LogException(new Exception(string.Format("Type {0} expected {1} received.", typeof(T1).Name, temp.GetType().Name), e));
-                    continue;
-                }
-
-                try
-                {
-                    if (arg == null || functor == null)
-                    {
-                        Debug.LogError("EzMsg: Signature Type Incompatible or Functor not assigned");
-                        return returnValue;
-                    }
-                    returnValue = functor(arg);
-                }
-                catch (Exception e)
-                {
-                    Debug.LogException(e);
-                }
-            }
-//            var handlerCount = internalHandlers.Count;
-//            s_HandlerListPool.Release(internalHandlers);
-//            if (handlerCount > 0)
-//                return returnValue;
-            return returnValue;
-        }
-
-        /// <summary>
-        /// Execute the specified event on the first game object underneath the current touch.
-        /// </summary>
-        private static readonly List<Transform> s_InternalTransformList = new List<Transform>(30);
-
-        public static GameObject ExecuteHierarchy<T>(GameObject root, EventAction<T> callbackAction) where T : IEventSystemHandler
-        {
-            GetEventChain(root, s_InternalTransformList);
-
-            for (var i = 0; i < s_InternalTransformList.Count; i++)
-            {
-                var transform = s_InternalTransformList[i];
-                if (Execute(transform.gameObject, callbackAction))
-                    return transform.gameObject;
-            }
-            return null;
-        }
-
-        private static bool ShouldSendToComponent<T>(Component component) where T : IEventSystemHandler
-        {
-            var valid = component is T;
-            if (!valid)
-                return false;
-
-            var behaviour = component as Behaviour;
-            if (behaviour != null)
-                return behaviour.enabled;
-            return true;
-        }
-
-        private static void GetEventList<T>(GameObject go, IList<IEventSystemHandler> results) where T : IEventSystemHandler
-        {
-            if (results == null)
-                throw new ArgumentException("Results array is null", "results");
-            if (go == null || !go.activeInHierarchy)
-                return;
-            List<Component> componentList = ListPool<Component>.Get();
-            go.GetComponents<Component>(componentList);
-            for (int index = 0; index < componentList.Count; ++index)
-            {
-                if (ShouldSendToComponent<T>(componentList[index]))
-                    results.Add(componentList[index] as IEventSystemHandler);
-            }
-            ListPool<Component>.Release(componentList);
-        }
-
-        internal static class ListPool<T>
-        {
-            private static readonly ObjectPool<List<T>> s_ListPool = new ObjectPool<List<T>>((UnityAction<List<T>>) null, (UnityAction<List<T>>) (l => l.Clear()));
-
-            public static List<T> Get()
-            {
-                return ListPool<T>.s_ListPool.Get();
-            }
-
-            public static void Release(List<T> toRelease)
-            {
-                ListPool<T>.s_ListPool.Release(toRelease);
-            }
-        }
 
         internal class ObjectPool<T> where T : new()
         {
@@ -493,35 +295,171 @@ namespace Ez.Msg
             }
         }
 
-//        /// <summary>
-//        /// Get the specified object's event event.
-//        /// </summary>
-//        private static void GetEventList<T>(GameObject go, IList<IEventSystemHandler> results) where T : IEventSystemHandler
-//        {
-//            // Debug.LogWarning("GetEventList<" + typeof(T).Name + ">");
-//            if (results == null)
-//                throw new ArgumentException("Results array is null", "results");
-//
-//            if (go == null || !go.activeInHierarchy)
-//                return;
-//
-//            var components = ComponentListPool.Get();
-//            go.GetComponents(components);
-//            for (var i = 0; i < components.Count; i++)
-//            {
-//                if (!ShouldSendToComponent<T>(components[i]))
-//                    continue;
-//
-//                // Debug.Log(string.Format("{2} found! On {0}.{1}", go, s_GetComponentsScratch[i].GetType(), typeof(T)));
-//                results.Add(components[i] as IEventSystemHandler);
-//            }
-//            ComponentListPool.Release(components);
-//            // Debug.LogWarning("end GetEventList<" + typeof(T).Name + ">");
-//        }
+        private static readonly ObjectPool<List<IEventSystemHandler>> s_HandlerListPool = new ObjectPool<List<IEventSystemHandler>>(null, l => l.Clear());
+
+        private static void GetEventChain(GameObject root, IList<Transform> eventChain)
+        {
+            eventChain.Clear();
+            if (root == null)
+                return;
+
+            var t = root.transform;
+            while (t != null)
+            {
+                eventChain.Add(t);
+                t = t.parent;
+            }
+        }
+
+        public static bool Execute<T>(GameObject target, EventAction<T> functor) where T : IEventSystemHandler
+        {
+            var internalHandlers = s_HandlerListPool.Get();
+            GetEventList<T>(target, internalHandlers);
+            //	if (s_InternalHandlers.Count > 0)
+            //		Debug.Log("Executing " + typeof (T) + " on " + target);
+
+            for (var i = 0; i < internalHandlers.Count; i++)
+            {
+                T arg;
+                try
+                {
+                    arg = (T)internalHandlers[i];
+                }
+                catch (Exception e)
+                {
+                    var temp = internalHandlers[i];
+                    Debug.LogException(new Exception(string.Format("Type {0} expected {1} received.", typeof(T).Name, temp.GetType().Name), e));
+                    continue;
+                }
+
+                try
+                {
+                    functor(arg);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogException(e);
+                }
+            }
+
+            var handlerCount = internalHandlers.Count;
+            s_HandlerListPool.Release(internalHandlers);
+            return handlerCount > 0;
+        }
+
+        /// <summary> Execute the specified event on the first game object underneath the current touch. </summary>
+        private static readonly List<Transform> s_InternalTransformList = new List<Transform>(30);
+
+        internal static class ListPool<T>
+        {
+            private static readonly ObjectPool<List<T>> s_ListPool = new ObjectPool<List<T>>((UnityAction<List<T>>) null, (UnityAction<List<T>>) (l => l.Clear()));
+
+            public static List<T> Get()
+            {
+                return ListPool<T>.s_ListPool.Get();
+            }
+
+            public static void Release(List<T> toRelease)
+            {
+                ListPool<T>.s_ListPool.Release(toRelease);
+            }
+        }
 
         /// <summary>
-        /// Whether the specified game object will be able to handle the specified event.
+        /// A Request sends a message asking for the second parameter type as return. Instead of a success/failure bool
+        /// For primitives you must use, eg. bool? or int? in the method call to get `null as return, if no answer found
         /// </summary>
+        /// <param name="target">Target GameObject to send the request</param>
+        /// <param name="eventData">Event Data</param>
+        /// <param name="functor">Actual Method to be executed</param>
+        /// <typeparam name="T1">Functor type</typeparam>
+        /// <typeparam name="T2">Return Type</typeparam>
+        /// <returns></returns>
+        public static T2 Request<T1,T2>(GameObject target, BaseEventData eventData, EventFunc<T1,T2> functor)
+            where T1 : IEventSystemHandler
+        {
+            var internalHandlers = s_HandlerListPool.Get();
+            GetEventList<T1>(target, internalHandlers);
+            // Debug Purposes Only
+//            Debug.Log(internalHandlers.Count + " scripts found of type " + typeof (T1) + " on " + target.name);
+//
+//            if (internalHandlers.Count > 0)
+//                Debug.Log("Executing " + typeof (T1) + " on " + target);
+
+            T2 returnValue = default(T2);
+            for (var i = 0; i < internalHandlers.Count; i++)
+            {
+                T1 arg;
+                try
+                {
+                    arg = (T1)internalHandlers[i];
+                }
+                catch (Exception e)
+                {
+                    var temp = internalHandlers[i];
+                    Debug.LogException(new Exception(string.Format("Type {0} expected {1} received.", typeof(T1).Name, temp.GetType().Name), e));
+                    continue;
+                }
+
+                try
+                {
+                    if (arg == null || functor == null)
+                    {
+                        Debug.LogError("EzMsg: Incompatible Signature Type or Functor not assigned");
+                        return returnValue;
+                    }
+                    returnValue = functor(arg);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogException(e);
+                }
+            }
+            return returnValue;
+        }
+
+        public static GameObject ExecuteHierarchy<T>(GameObject root, EventAction<T> callbackAction) where T : IEventSystemHandler
+        {
+            GetEventChain(root, s_InternalTransformList);
+
+            for (var i = 0; i < s_InternalTransformList.Count; i++)
+            {
+                var transform = s_InternalTransformList[i];
+                if (Execute(transform.gameObject, callbackAction))
+                    return transform.gameObject;
+            }
+            return null;
+        }
+
+        private static bool ShouldSendToComponent<T>(Component component) where T : IEventSystemHandler
+        {
+            var valid = component is T;
+            if (!valid)
+                return false;
+
+            var behaviour = component as Behaviour;
+            if (behaviour != null)
+                return behaviour.enabled;
+            return true;
+        }
+
+        private static void GetEventList<T>(GameObject go, IList<IEventSystemHandler> results) where T : IEventSystemHandler
+        {
+            if (results == null)
+                throw new ArgumentException("Results array is null", "results");
+            if (go == null || !go.activeInHierarchy)
+                return;
+            List<Component> componentList = ListPool<Component>.Get();
+            go.GetComponents<Component>(componentList);
+            for (int index = 0; index < componentList.Count; ++index)
+            {
+                if (ShouldSendToComponent<T>(componentList[index]))
+                    results.Add(componentList[index] as IEventSystemHandler);
+            }
+            ListPool<Component>.Release(componentList);
+        }
+
+        /// <summary> Whether the specified game object will be able to handle the specified event. </summary>
         public static bool CanHandleEvent<T>(GameObject go) where T : IEventSystemHandler
         {
             var internalHandlers = s_HandlerListPool.Get();
@@ -531,9 +469,7 @@ namespace Ez.Msg
             return handlerCount != 0;
         }
 
-        /// <summary>
-        /// Bubble the specified event on the game object, figuring out which object will actually receive the event.
-        /// </summary>
+        /// <summary> Bubble the specified event on the game object, figuring out which object will actually receive the event. </summary>
         public static GameObject GetEventHandler<T>(GameObject root) where T : IEventSystemHandler
         {
             if (root == null)
